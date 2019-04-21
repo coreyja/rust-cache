a747f552c9628d8355b735bd57a28042264dcca4
Corey Alexander <coreyja@gmail.com>
2019-04-21 17:11:40 -0400

    Get simple tar.gqz done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+extern crate flate2;
+extern crate tar;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::fs::File;
+
+fn main() -> Result<(), std::io::Error> {
+    let tar_gz = File::create("archive.tar.gz")?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("./cache", "./cache")?;
+    Ok(())
 }
