f8e1dd0fa7b1b6a0ec2d8fcc2553bfd64d705611
Corey Alexander <coreyja@gmail.com>
2019-04-25 23:01:09 -0400

    Update ruby script

--- ruby.sh
+++ ruby.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
-md5sum Gemfile.lock | awk '{ print $1 }' > .cache_key
+cache_version="1"
+gemfile_hash=$(md5sum Gemfile.lock | awk '{ print $1 }')
+
+echo "v$cache_version-$gemfile_hash" > .cache_key
 
 CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
   CACHE_BUCKET=$PLUGIN_CACHE_BUCKET \
   CACHE_LOCAL_DIR="vendor/bundle" \
   /usr/local/rust-cache
+
+rm .cache_key
