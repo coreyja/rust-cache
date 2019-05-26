f71ba8412d508192910d9e8a4008b0564e6a6a51
Corey Alexander <coreyja@gmail.com>
2019-05-26 19:28:40 -0400

    Try to just do the top level

--- cache-types/rust/rust.sh
+++ cache-types/rust/rust.sh
@@ -9,14 +9,14 @@ echo "rust-v$cache_version-$lock_hash" > .cache_key
 if [[ -z "${PLUGIN_CACHE_DOWNLOAD}" ]]; then
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="target,.cargo/registry" \
+    CACHE_LOCAL_DIR="target,.cargo" \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
     /usr/local/rust-cache
 else
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="target,.cargo/registry" \
+    CACHE_LOCAL_DIR="target,.cargo" \
     CACHE_DOWNLOAD=1 \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
