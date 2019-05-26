1aeb4a930c9e8e4575911e5212fc7f70c64c63f7
Corey Alexander <coreyja@gmail.com>
2019-05-26 17:18:56 -0400

    Make this self contained by using yarnrc file for cache location

--- cache-types/yarn/yarn.sh
+++ cache-types/yarn/yarn.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-cache_version="5"
+cache_version="6"
 yarn_hash=$(md5sum yarn.lock | awk '{ print $1 }')
+yarn_cache_location="cache/yarn"
 
 echo "yarn-v$cache_version-$yarn_hash" > .cache_key
+echo "--cache-folder $yarn_cache_location" >> .yarnrc
 
 if [[ -z "${PLUGIN_CACHE_DOWNLOAD}" ]]; then
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="cache/yarn,node_modules,.yarn" \
+    CACHE_LOCAL_DIR="$yarn_cache_location" \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
     /usr/local/rust-cache
 else
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="cache/yarn,node_modules,.yarn" \
+    CACHE_LOCAL_DIR="$yarn_cache_location" \
     CACHE_DOWNLOAD=1 \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
