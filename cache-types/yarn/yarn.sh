67f039da89bcff398ccef6ad96f744ca9437f856
Corey Alexander <coreyja@gmail.com>
2019-05-26 16:37:00 -0400

    Try caching node modules and yarn cache dir

--- cache-types/yarn/yarn.sh
+++ cache-types/yarn/yarn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cache_version="4"
+cache_version="5"
 yarn_hash=$(md5sum yarn.lock | awk '{ print $1 }')
 
 echo "yarn-v$cache_version-$yarn_hash" > .cache_key
@@ -9,14 +9,14 @@ echo "yarn-v$cache_version-$yarn_hash" > .cache_key
 if [[ -z "${PLUGIN_CACHE_DOWNLOAD}" ]]; then
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="cache/yarn" \
+    CACHE_LOCAL_DIR="cache/yarn,node_modules" \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
     /usr/local/rust-cache
 else
   CACHE_S3_PATH="$DRONE_REPO_OWNER/$DRONE_REPO_NAME/" \
     CACHE_BUCKET="$PLUGIN_CACHE_BUCKET" \
-    CACHE_LOCAL_DIR="cache/yarn" \
+    CACHE_LOCAL_DIR="cache/yarn,node_modules" \
     CACHE_DOWNLOAD=1 \
     AWS_ACCESS_KEY_ID="$PLUGIN_AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$PLUGIN_AWS_SECRET_ACCESS_KEY" \
